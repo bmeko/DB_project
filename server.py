33ecade15addb8760138db43f46616b63f19541c
bmeko <48912701+bmeko@users.noreply.github.com>
2020-10-06 15:13:58 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,46 @@
+import tornado.web
+import tornado.ioloop
+
+class registerRequestHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        
+        self.render("register.html")
+
+    def post(self):
+        fname = self.get_argument('fname',"")
+        lname = self.get_argument('lname',"")
+        position = self.get_argument('positon',"")
+        email = self.get_argument('email')
+        pas = self.get_argument('password',"")
+        cpas= self.get_argument('conpassword',"")
+        phone= self.get_argument('phone',"")
+
+        ### password confirmation check ###
+        
+        self.write("Wow " + fname + " you're a " + position +" "+email+" "+phone)
+class loginRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        email = self.get_argument('email',"")
+        password = self.get_argument('password',"")
+
+        self.write("email " +email+" password "+password)
+
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("loginpage.html")
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/register", registerRequestHandler),
+        (r"/", listRequestHandler),
+        (r"/login", loginRequestHandler)
+    ])
+    
+    port = 8877
+    app.listen(port)
+    print("Application is ready and listening on port {}".format(port))
+    tornado.ioloop.IOLoop.current().start()
